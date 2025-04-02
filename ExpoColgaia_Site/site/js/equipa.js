9b7585178fc9cc78f2b751b8a466fbf0df76a23d
inokas6 <163462161+inokas6@users.noreply.github.com>
2025-04-02 08:37:01 +0100

    Delete ExpoColgaia_Site/site directory

--- ExpoColgaia_Site/site/js/equipa.js
+++
@@ -1,9 +0,0 @@
-const cardsContainer = document.querySelector('.cards');
-
-function moveLeft() {
-  cardsContainer.scrollBy({ left: -350, behavior: 'smooth' });
-}
-
-function moveRight() {
-  cardsContainer.scrollBy({ left: 350, behavior: 'smooth' });
-}
